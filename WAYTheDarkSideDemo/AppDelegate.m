07de45b7cfd78415018b04b5df5f315ee516d4d1
Raffael Hannemann <gitlab@raffael.me>
2014-11-18 13:16:30 +0100

    Initial Commit

---
+++ WAYTheDarkSideDemo/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  WAYTheDarkSideDemo
+//
+//  Created by Raffael Hannemann on 18.11.14.
+//  Copyright (c) 2014 We Are Yeah!. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+	// Insert code here to tear down your application
+}
+
+@end
---
+++ WAYTheDarkSideDemo/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  WAYTheDarkSideDemo
+//
+//  Created by Raffael Hannemann on 18.11.14.
+//  Copyright (c) 2014 We Are Yeah!. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+	return NSApplicationMain(argc, argv);
+}
